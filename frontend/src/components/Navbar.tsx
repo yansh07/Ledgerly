3b7d5df5dbc83e5de8dd4be0476d345b43445991
pksingh <pksingh69313@gmail.com>
2026-02-21 01:47:46 +0530

    removed unnecessary buttons

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,6 +1,4 @@
 import { LuReceiptIndianRupee } from "react-icons/lu";
-import { FaGoogle } from "react-icons/fa";
-import { setUserAuthenticated } from "../utils/auth";
 
 function Navbar() {
   return (
@@ -15,15 +13,6 @@ function Navbar() {
               <span className="text-2xl md:text-3xl font-black tracking-tight">Ledgerly</span>
             </a>
           </div>
-          <button className="hidden md:block px-2 md:px-8 py-3 rounded-lg bg-gradient-to-r from-amber-500 to-orange-500 text-slate-950 font-bold text-sm md:text-base hover:shadow-lg hover:shadow-amber-500/50 transition-all duration-300 hover:scale-105">
-            <a
-              href="/auth/google"
-              onClick={() => setUserAuthenticated(true)}
-              className="no-underline flex items-center gap-2 md:text-xl text-lg"
-            >
-              Continue with <span><FaGoogle className="text-lg"/></span>
-            </a>
-          </button>
         </nav>
       </div>
       <div className="border-b border-gray-800 mt-6" />
