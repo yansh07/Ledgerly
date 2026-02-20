c69fb4a8704633b22a2224d66240a564500aeffe
pksingh <pksingh69313@gmail.com>
2026-02-21 02:25:16 +0530

    Added apis to add and delete expense

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,10 +4,13 @@ from google.oauth2 import id_token
 from google.auth.transport import requests as google_requests
 import os
 from app.db import get_db
-from app.models import User
+from app.models import User, Expense
 from app.utils.auth_utils import create_access_token
 from dotenv import load_dotenv
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from datetime import date
+from app.utils.auth_utils import get_current_user
 
 load_dotenv()
 
@@ -69,3 +72,43 @@ async def google_auth(payload: dict, db: Session = Depends(get_db)):
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Invalid google token"
         )
+    
+class ExpenseSchema(BaseModel):
+    category: str
+    amount: float
+    date: date
+
+@app.post("/expense")
+async def add_expense(
+    data: ExpenseSchema,
+    db: Session = Depends(get_db),
+    user: User = Depends(get_current_user)
+):
+    new_exp = Expense(
+        user_id=user.id,
+        category=data.category,
+        amount=data.amount,
+        date=data.date,
+    )
+    db.add(new_exp)
+    db.commit()
+    return {"status": "success", "data": new_exp}
+
+@app.get("/expense")
+async def get_expense(db: Session = Depends(get_db), user: User = Depends(get_current_user)):
+    expenses = db.query(Expense).filter(Expense.user_id == user.id).all()
+    return expenses
+
+@app.delete("/expense/{expense_id}")
+async def delete_expense(
+    expense_id: int,
+    db: Session = Depends(get_db),
+    user: User = Depends(get_current_user)
+):
+    expense = db.query(Expense).filter(Expense.id == expense_id, Expense.user_id == user.id).first()
+    if not expense:
+        raise HTTPException(status_code=404, detail="Expense not found")
+    
+    db.delete(expense)
+    db.commit()
+    return {"status": "success", "message": "Expense deleted successfully"}
--- backend/app/utils/auth_utils.py
+++ backend/app/utils/auth_utils.py
@@ -1,9 +1,16 @@
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
 from dotenv import load_dotenv
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from app.db import get_db
+from app.models import User
+from sqlalchemy.orm import Session
 import os
 
 load_dotenv()
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/google")
 
 secret_key: str = os.getenv("JWT_SECRET", "")
 if not secret_key:
@@ -25,3 +32,14 @@ def verify_token(token: str):
         return payload.get("sub")
     except JWTError:
         return None
+    
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    email = verify_token(token)
+    if email is None:
+        raise HTTPException(status_code=401, detail="Invalid Session")
+    
+    user = db.query(User).filter(User.email == email).first()
+
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
